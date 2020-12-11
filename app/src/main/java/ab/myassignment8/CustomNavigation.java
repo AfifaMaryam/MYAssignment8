780bfe2266339cad75130c41e7b2ae89a01242eb
AfifaMaryam <razakwt22@gmail.com>
2020-12-11 20:04:49 +0500

    Initial commit

---
+++ app/src/main/java/ab/myassignment8/CustomNavigation.java
@@ -0,0 +1,37 @@
+package ab.myassignment8;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+
+public class CustomNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_navigation);
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawerLayout=findViewById(R.id.drawerLayout);
+
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(
+                this, drawerLayout,toolbar, R.string.drawer_open,R.string.drawer_close
+
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+    }
+   
+
+
+}
